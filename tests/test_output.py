920fa3069ef232466cd2b28837f10439f976f9d2
faysal887 <faysalaslam887@gmail.com>
2024-03-06 13:37:30 +0100

    added test cases

---
+++ tests/test_output.py
@@ -0,0 +1,58 @@
+import sys, os, json
+
+# Get the absolute path to the 'visable' directory
+visable_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Get two levels up
+sys.path.append(visable_dir)  # Add 'visable' to the path
+
+# Now you can import 'solution' modules
+from endpoint import app, load_model, predict_fasttext, classify
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_load_model():
+    load_model() 
+
+
+def test_classify_endpoint():
+    text_data = {"text": "zucker"}
+    response = client.post("/classify/", json=text_data)
+    assert response.status_code == 200
+    assert isinstance(json.loads(response.text), dict)
+
+
+def test_low_confidence():
+    text_data = {"text": "helloworld"}
+    response = client.post("/classify/", json=text_data)
+    assert response.status_code == 200
+    assert json.loads(response.text)['label'] == 'undefined'
+
+
+def test_invalid_data_type():
+    text_data = {"text": 5}
+    response = client.post("/classify/", json=text_data)
+    result = json.loads(response.text)
+    assert response.status_code == 422
+    assert result['detail'][0]['type']=='string_type'
+    assert result['detail'][0]['msg']=='Input should be a valid string'
+
+
+def test_long_input():
+    text_data = {"text": 'x'*2048}
+    response = client.post("/classify/", json=text_data)
+    result = json.loads(response.text)
+    assert response.status_code == 422
+    assert result['detail'][0]['type']=='string_too_long'
+    assert result['detail'][0]['msg']=='String should have at most 1024 characters'
+
+
+def test_short_input():
+    text_data = {"text": 'x'}
+    response = client.post("/classify/", json=text_data)
+    result = json.loads(response.text)
+    assert response.status_code == 422
+    assert result['detail'][0]['type']=='string_too_short'
+    assert result['detail'][0]['msg']=='String should have at least 2 characters'
+
+
+
